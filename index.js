5e826b6a384ced6af1c2c0b30de0204d7b60bd90
Ahmed Ayman <ahmedayman4a77@gmail.com>
2020-04-03 17:58:08 +0200

    Remove autoupdate , hide to tray , added a context menu

--- index.js
+++ index.js
@@ -1,59 +1,129 @@
-const { app, BrowserWindow,ipcMain  } = require('electron');
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Tray,
+  Menu
+} = require('electron');
 const path = require('path');
-const isDev = require('electron-is-dev');
-if (!isDev) {
-	require('update-electron-app')()
-}
+const contextMenu = require('electron-context-menu');
+
+contextMenu({
+	prepend: (defaultActions, params, mainWindow) => [
+		{
+			label: 'Back',
+			click: () => {
+				mainWindow.webContents.goBack();
+			}
+		},
+		{
+			label: 'Forward',
+			click: () => {
+				mainWindow.webContents.goForward();
+			}
+		}
+	]
+});
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    center:true,
-    show:false,
-    frame:false,
-    maximizable: true,
-    icon:path.join(__dirname,'img/icon.png'),
-    backgroundColor: '#2e2c29',
-    webPreferences: {
-        preload:path.join(__dirname,'renderer.js')
+let isQuiting;
+let tray;
+let mainWindow = null;
+
+const gotTheLock = app.requestSingleInstanceLock();
+// Allow only one instance of the app
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (!mainWindow.isVisible()) mainWindow.show()
+      mainWindow.focus()
     }
-  });
+  })
 
-  // and load the home page of the app.
-  mainWindow.loadURL('https://onehack.us');
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', () => {
+    // Create the browser window.
+   mainWindow = new BrowserWindow({
+      center: true,
+      show: false,
+      frame: false,
+      maximizable: true,
+      icon: path.join(__dirname, 'img/icon.png'),
+      backgroundColor: '#2e2c29',
+      webPreferences: {
+        preload: path.join(__dirname, 'renderer.js')
+      }
+    });
 
-  //when ready maximize and show. This reduces flashing
-  
-  ipcMain.on('App-Loaded', ()=>{
-    mainWindow.maximize();
-    mainWindow.show();
-  });
+    tray = new Tray(path.join(__dirname, 'img/icon.png'));
+    tray.setIgnoreDoubleClickEvents(true)
+    tray.setContextMenu(Menu.buildFromTemplate([{
+        label: 'Show App',
+        click: function () {
+          mainWindow.show();
+        }
+      },
+      {
+        label: 'Quit',
+        click: function () {
+          isQuiting = true;
+          app.quit();
+        }
+      }
+    ]));
 
-  var handleRedirect = (e, url) => {
-    mainWindow.webContents.send('PageChanged',"Page has changed");
-    if(url != mainWindow.webContents.getURL()) {
-      e.preventDefault();
-      require('electron').shell.openExternal(url);
+    tray.setToolTip('1Hack - Together WE Learn');
+
+    // and load the home page of the app.
+    mainWindow.loadURL('https://onehack.us');
+
+    //when ready maximize and show. This reduces flashing
+
+    ipcMain.on('App-Loaded', () => {
+      mainWindow.maximize();
+      mainWindow.show();
+    });
+
+    //Open all links with target=_blank in the default browser
+    var handleRedirect = (e, url) => {
+      mainWindow.webContents.send('PageChanged', "Page has changed");
+      if (url != mainWindow.webContents.getURL()) {
+        e.preventDefault();
+        require('electron').shell.openExternal(url);
+      }
     }
-  }
-  
-  mainWindow.webContents.on('new-window', handleRedirect);
-};
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+    mainWindow.webContents.on('new-window', handleRedirect);
+
+    mainWindow.on('close', function (event) {
+      if (!isQuiting) {
+        event.preventDefault();
+        mainWindow.hide();
+        event.returnValue = false;
+      }
+    });
+  });
+}
+
+app.on('before-quit', function () {
+  isQuiting = true;
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
+    isQuiting = true;
     app.quit();
   }
 });
@@ -65,4 +135,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
